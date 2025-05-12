6285bef7bbf6b7a7f73e4d79c223438d2a252fcc
pedro0527 <sooyong0527@naver.com>
2025-05-12 17:29:01 +0900

    [chore]/#5 - Color Assets 적용 테스트

--- TODOMate/TODOMate/Features/ViewController.swift
+++ TODOMate/TODOMate/Features/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .greenCategory1
         // Do any additional setup after loading the view.
     }
 }

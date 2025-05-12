246c0ed57bc4ae25e454106028b716dee9439d76
hyeonju <shj040201@naver.com>
2025-05-12 20:12:49 +0900

    [add]/#3 - BaseUiView, ViewController 추가

--- TODOMate/TODOMate/Global/Base/BaseUIView.swift
+++ TODOMate/TODOMate/Global/Base/BaseUIView.swift
@@ -5,4 +5,21 @@
 //  Created by 성현주 on 5/8/25.
 //
 
-import Foundation
+import UIKit
+
+class BaseUIView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setLayout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func setUI() {}
+
+    func setLayout() {}
+}

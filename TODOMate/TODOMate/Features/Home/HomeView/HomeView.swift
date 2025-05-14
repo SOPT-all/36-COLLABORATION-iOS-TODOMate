9b7226de3a968b16361cf1078d986c8568dc4dc2
hyeonju <shj040201@naver.com>
2025-05-15 03:17:42 +0900

    [feat]/#13 - homeview 레이아웃 조정

--- TODOMate/TODOMate/Features/Home/HomeView/HomeView.swift
+++ TODOMate/TODOMate/Features/Home/HomeView/HomeView.swift
@@ -5,7 +5,7 @@
 //  Created by 성현주 on 5/12/25.
 //
 
-import Foundation
+import UIKit
 
 final class HomeView: BaseUIView {
 
@@ -13,21 +13,34 @@ final class HomeView: BaseUIView {
 
     let navigationBar = CustomNavigationBar()
 
+    private let scrollView = UIScrollView()
+    private let contentView = UIView()
+
     let categoryButton1 = CapsuleButton().then {
         $0.type = .category
         $0.text = "카테고리 1"
         $0.textColor = .greenCategory1
     }
 
-    let todoListView = TodoListView() 
+    let todoListView1 = TodoListView()
 
     let categoryButton2 = CapsuleButton().then {
-        $0.type = .toolBar
-        $0.text = "루틴등록"
-        $0.leftIcon = .reIconRoutinGrey
+        $0.type = .category
+        $0.text = "카테고리 2"
+        $0.textColor = .redCategory2
     }
 
+    let todoListView2 = TodoListView()
+
     let categoryButton3 = CapsuleButton().then {
+        $0.type = .category
+        $0.text = "카테고리 3"
+        $0.textColor = .redCategory3
+    }
+
+    let todoListView3 = TodoListView()
+
+    let aiButton = CapsuleButton().then {
         $0.type = .ai
         $0.text = "AI 할일생성"
     }
@@ -35,7 +48,15 @@ final class HomeView: BaseUIView {
     // MARK: - Custom Methods
 
     override func setUI() {
-        addSubviews(navigationBar, categoryButton1, todoListView, categoryButton2, categoryButton3)
+        addSubviews(navigationBar, scrollView)
+        scrollView.addSubview(contentView)
+
+        contentView.addSubviews(
+            categoryButton1, todoListView1,
+            categoryButton2, todoListView2,
+            categoryButton3, todoListView3,
+            aiButton
+        )
     }
 
     override func setLayout() {
@@ -45,24 +66,58 @@ final class HomeView: BaseUIView {
             $0.height.equalTo(48)
         }
 
-        categoryButton1.snp.makeConstraints {
-            $0.top.equalTo(navigationBar.snp.bottom).offset(16)
-            $0.centerX.equalToSuperview()
+        scrollView.snp.makeConstraints {
+            $0.top.equalTo(navigationBar.snp.bottom)
+            $0.leading.trailing.bottom.equalToSuperview()
         }
 
-        todoListView.snp.makeConstraints {
+        contentView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+            $0.width.equalTo(scrollView)
+        }
+
+        categoryButton1.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(200)
+            $0.leading.equalToSuperview().inset(20)
+        }
+
+        todoListView1.snp.makeConstraints {
             $0.top.equalTo(categoryButton1.snp.bottom).offset(20)
             $0.leading.trailing.equalToSuperview().inset(20)
         }
 
         categoryButton2.snp.makeConstraints {
-            $0.top.equalTo(todoListView.snp.bottom).offset(20)
-            $0.centerX.equalToSuperview()
+            $0.top.equalTo(todoListView1.snp.bottom).offset(20)
+            $0.leading.equalToSuperview().inset(20)
+        }
+
+        todoListView2.snp.makeConstraints {
+            $0.top.equalTo(categoryButton2.snp.bottom).offset(20)
+            $0.leading.trailing.equalToSuperview().inset(20)
         }
 
         categoryButton3.snp.makeConstraints {
-            $0.top.equalTo(categoryButton2.snp.bottom).offset(20)
-            $0.centerX.equalToSuperview()
+            $0.top.equalTo(todoListView2.snp.bottom).offset(20)
+            $0.leading.equalToSuperview().inset(20)
+        }
+
+        todoListView3.snp.makeConstraints {
+            $0.top.equalTo(categoryButton3.snp.bottom).offset(20)
+            $0.leading.trailing.equalToSuperview().inset(20)
+        }
+
+        aiButton.snp.makeConstraints {
+            $0.top.equalTo(todoListView3.snp.bottom).offset(20)
+            $0.leading.equalToSuperview().inset(20)
+            $0.bottom.equalToSuperview().inset(40)
+        }
+    }
+}
+
+extension HomeView {
+    func addSubTaskToFocusedList() {
+        [todoListView1, todoListView2, todoListView3].forEach {
+            $0.addSubTaskToFocused()
         }
     }
 }

4845afa926ed388dd431985d52d7ea2fb47be872
yeonee <leenayeon@sookmyung.ac.kr>
2025-05-14 22:59:02 +0900

    [add]/#15 - custom font 추가

--- TODOMate/TODOMate/Global/Extensions/UIFont+.swift
+++ TODOMate/TODOMate/Global/Extensions/UIFont+.swift
@@ -8,33 +8,46 @@
 import UIKit
 
 extension UIFont {
-    class var body_med_16: UIFont {
-        return UIFont(name: "Pretendard-Medium", size: 23)!
+    class var body_bold_14: UIFont {
+        return UIFont(name: "Pretendard-Bold", size: 13)!
     }
+    
+    class var body_med_16: UIFont {
+        return UIFont(name: "Pretendard-Medium", size: 16)!
+    }
+    
     class var body_semi_14: UIFont {
         return UIFont(name: "Pretendard-SemiBold", size: 14)!
     }
+    
     class var body_reg_14: UIFont {
         return UIFont(name: "Pretendard-Regular", size: 14)!
     }
+    
     class var cap_bold_12: UIFont {
         return UIFont(name: "Pretendard-Bold", size: 12)!
     }
+    
     class var cap_semi_12: UIFont {
         return UIFont(name: "Pretendard-SemiBold", size: 12)!
     }
+    
     class var cap_med_12: UIFont {
         return UIFont(name: "Pretendard-Medium", size: 12)!
     }
+    
     class var cap_reg_12: UIFont {
         return UIFont(name: "Pretendard-Regular", size: 12)!
     }
+    
     class var cap_semi_10: UIFont {
         return UIFont(name: "Pretendard-SemiBold", size: 10)!
     }
+    
     class var cap_medi_10: UIFont {
         return UIFont(name: "Pretendard-Medium", size: 10)!
     }
+    
     class var cap_bold_8: UIFont {
         return UIFont(name: "Pretendard-Bold", size: 8)!
     }

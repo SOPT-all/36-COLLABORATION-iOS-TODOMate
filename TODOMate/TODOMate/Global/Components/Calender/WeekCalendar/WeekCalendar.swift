b326ba4d71af68163d91ac031463295aa7f1e984
yeonee <leenayeon@sookmyung.ac.kr>
2025-05-16 01:34:52 +0900

    [feat]/#19 - 주간달력 ui 및 날짜 연결

---
+++ TODOMate/TODOMate/Global/Components/Calender/WeekCalendar/WeekCalendar.swift
@@ -0,0 +1,147 @@
+//
+//  WeekCalendar.swift
+//  TODOMate
+//
+//  Created by 이나연 on 5/15/25.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class WeekCalendar: BaseUIView {
+
+    // MARK: - Properties
+    
+        private let formatter = CalendarDateFormatter()
+        private var weekDates: [String] = []
+        private lazy var selectedIndex = formatter.getTodayDay()
+    
+    // MARK: - UI Componets
+    
+    private let weekStackView = UIStackView().then {
+        $0.axis = .horizontal
+        $0.spacing = 29
+        $0.distribution = .fillEqually
+    }
+    
+    private let mondayLabel = CalendarDayLabel().then {
+        $0.text = "월"
+    }
+    
+    private let tuesdayLabel = CalendarDayLabel().then {
+        $0.text = "화"
+    }
+    
+    private let wednesdayLabel = CalendarDayLabel().then {
+        $0.text = "수"
+    }
+    
+    private let thursdayLabel = CalendarDayLabel().then {
+        $0.text = "목"
+    }
+    
+    private let fridayLabel = CalendarDayLabel().then {
+        $0.text = "금"
+    }
+    
+    private let saturdayLabel = CalendarDayLabel(weekday: "saturday").then {
+        $0.text = "토"
+        $0.textColor = .blue20
+    }
+    
+    private let sundayLabel = CalendarDayLabel(weekday: "sunday").then {
+        $0.text = "일"
+        $0.textColor = .red10
+    }
+    
+    private let weekCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    
+    // MARK: - SetUp Method
+    
+    override func setUI() {
+        addSubviews(weekStackView, weekCollectionView)
+        [mondayLabel, tuesdayLabel, wednesdayLabel, thursdayLabel, fridayLabel, saturdayLabel, sundayLabel].forEach {
+            weekStackView.addArrangedSubview($0)
+        }
+        
+        setWeekCollectionViewFlowLayout()
+        registerWeekCollectionViewCell()
+        setDate()
+    }
+    
+    override func setLayout() {
+        weekStackView.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(20)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(21)
+        }
+        
+        weekCollectionView.snp.makeConstraints {
+            $0.top.equalTo(weekStackView.snp.bottom)
+            $0.leading.trailing.equalToSuperview().inset(2)
+            $0.height.equalTo(60)
+        }
+    }
+    
+    private func setWeekCollectionViewFlowLayout() {
+        let flowLayout = UICollectionViewFlowLayout()
+        let cellHeight: CGFloat = 39
+        let spacing: CGFloat = 35
+        let cellWidth: CGFloat = 20
+        
+        flowLayout.itemSize = CGSize(width: cellWidth, height: cellHeight)
+        flowLayout.scrollDirection = .horizontal
+        flowLayout.collectionView?.isScrollEnabled = false
+        flowLayout.minimumLineSpacing = spacing
+        self.weekCollectionView.setCollectionViewLayout(flowLayout, animated: false)
+        
+    }
+    
+    private func registerWeekCollectionViewCell() {
+        weekCollectionView.register(WeekCollectionViewCell.self, forCellWithReuseIdentifier: WeekCollectionViewCell.identifier)
+        weekCollectionView.delegate = self
+        weekCollectionView.dataSource = self
+    }
+    
+    private func setDate() {
+        weekDates = formatter.getDateStringsOfThisWeek()
+        weekCollectionView.reloadData()
+    }
+}
+
+extension WeekCalendar: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: WeekCollectionViewCell.identifier, for: indexPath) as? WeekCollectionViewCell else {
+            return WeekCollectionViewCell()
+        }
+        
+        let dateText = weekDates[indexPath.item]
+        let isToday = dateText == formatter.getTodayDate()
+        let isSelected = selectedIndex == indexPath.item
+        let isSaturday = indexPath.item == 5
+        let isSunday = indexPath.item == 6
+        print("오늘날짜는?", dateText)
+        print("오늘날짜인가요?", isToday)
+        print("선택되었나요?", isSelected)
+        print("토요일인가요?", isSaturday)
+        print("일요일?", isSunday)
+        cell.dataBind(date: dateText, isToday: isToday, isSelected: isSelected, isSaturday: isSaturday, isSunday: isSunday)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 7
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        selectedIndex = indexPath.item
+        print("바뀐 인덱스", selectedIndex)
+        weekCollectionView.reloadData()
+    }
+}
+
+extension WeekCalendar: UICollectionViewDelegate {
+    
+}
---
+++ TODOMate/TODOMate/Global/Components/Calender/WeekCalendar/WeekCollectionViewCell.swift
@@ -0,0 +1,88 @@
+//
+//  WeekCollectionViewCell.swift
+//  TODOMate
+//
+//  Created by 이나연 on 5/15/25.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class WeekCollectionViewCell: UICollectionViewCell {
+    static let identifier = "WeekCollectionViewCell"
+    
+    // MARK: - UI Components
+    
+    private let checkIconButton = UIButton().then {
+        $0.setBackgroundImage(.reIconWeekdayUnchecked, for: .normal)
+        $0.clipsToBounds = true
+        $0.contentMode = .scaleAspectFit
+        $0.isUserInteractionEnabled = false
+    }
+    
+    private let dayLabel = CalendarDayLabel().then {
+        $0.text = "0"
+    }
+    
+    // MARK: - Setup Method
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUI() {
+        [checkIconButton, dayLabel].forEach {
+            contentView.addSubview($0)
+        }
+    }
+    
+    private func setLayout() {
+        checkIconButton.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.leading.trailing.equalToSuperview()
+            $0.width.height.equalTo(20)
+        }
+        
+        dayLabel.snp.makeConstraints {
+            $0.top.equalTo(checkIconButton.snp.bottom).offset(7)
+            $0.leading.trailing.equalToSuperview()
+            $0.width.height.equalTo(19)
+        }
+    }
+}
+
+extension WeekCollectionViewCell {
+    func dataBind(date: String, isToday: Bool, isSelected: Bool, isSaturday: Bool, isSunday: Bool) {
+        dayLabel.text = date
+        
+        if isSaturday {
+            dayLabel.textColor = .blue20
+        } else if isSunday {
+            dayLabel.textColor = .red10
+        }
+
+        if isSelected {
+            changeCellBackgroundColor()
+        } else if isToday {
+            changeCellBackgroundColor()
+        } else {
+            contentView.backgroundColor = .clear
+        }
+    }
+    
+    func changeCellBackgroundColor() {
+        dayLabel.backgroundColor = .black
+        dayLabel.layer.cornerRadius = 10
+        dayLabel.clipsToBounds = true
+        dayLabel.textColor = .white
+    }
+}

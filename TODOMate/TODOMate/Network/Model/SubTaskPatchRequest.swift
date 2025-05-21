88b14c6d037fcf6eeb83c9c1f34cd3058687c612
yeonee <leenayeon@sookmyung.ac.kr>
2025-05-22 03:20:14 +0900

    [feat]/#27-서브태스크 완료 api

---
+++ TODOMate/TODOMate/Network/Model/SubTaskPatchRequest.swift
@@ -0,0 +1,12 @@
+//
+//  SubTaskPatchResponse.swift
+//  TODOMate
+//
+//  Created by 이나연 on 5/22/25.
+//
+
+import Foundation
+
+struct SubTaskPatchRequest: Encodable {
+    let completed: Bool
+}
---
+++ TODOMate/TODOMate/Network/Service/SubTaskPatchService.swift
@@ -0,0 +1,24 @@
+//
+//  SubTaskPatchService.swift
+//  TODOMate
+//
+//  Created by 이나연 on 5/22/25.
+//
+
+struct VoidType: Decodable {}
+
+final class SubTaskPatchService {
+    let shared = BaseService.shared
+    
+    func patchSubTask(id: Int, request: [SubTaskPatchRequest]) async throws -> BaseResponse<VoidType> {
+        do {
+            let response: BaseResponse<VoidType> = try await shared.request(
+                endPoint: .patchSubTasks(id),
+                body: request
+            )
+            return response
+        } catch {
+            throw error
+        }
+    }
+}

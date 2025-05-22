4cf488dd199efa092c878c4e581b7953ab638d7e
pedro0527 <sooyong0527@naver.com>
2025-05-22 20:09:49 +0900

    [feat]/#31 - 코드 1차 합치기

--- TODOMate/TODOMate/Network/Service/MainCompletedService.swift
+++ TODOMate/TODOMate/Network/Service/MainCompletedService.swift
@@ -5,14 +5,12 @@
 //  Created by LEESOOYONG on 5/22/25.
 //
 
-struct VoidType: Decodable {}
-
 final class MainCompletedService {
     let shared = BaseService.shared
     
-    func patchMainTask(id: Int, request: MainCompletedRequest) async throws -> BaseResponse<VoidType> {
+    func patchMainTask(id: Int, request: MainCompletedRequest) async throws -> VoidType {
         do {
-            let response: BaseResponse<VoidType> = try await shared.request(
+            let response: VoidType = try await shared.request(
                 endPoint: .patchMainTasks(id),
                 body: request
             )
